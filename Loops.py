d3251162acd3ddec585376b5dc91ec943aacf6a3
TUMWESIGYE ALEX <93213466+Tumwesigye-alex@users.noreply.github.com>
2022-03-04 14:07:13 +0300

    loops in python
    Ease functions with loops to iterate through our programs

---
+++ Loops.py
@@ -0,0 +1,10 @@
+max_temp=102.5
+
+temperature=float(input("Enter the temperature in degrees"))
+print("The temperature is \t",temperature)
+while temperature > max_temp:
+    print("The temperature is too high\n Its terrible\n You might die\n Better run away")
+   
+    count=10
+    while count > 10:
+        print("Hello")
