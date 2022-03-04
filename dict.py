d7866bcdf2ae96319d113e927f5f0058c327af85
TUMWESIGYE ALEX <93213466+Tumwesigye-alex@users.noreply.github.com>
2022-03-04 14:04:43 +0300

    dictionaries in python
    Well explained source code for dictionaries

---
+++ dict.py
@@ -0,0 +1,12 @@
+#programthatillustrates the use of dictionaries
+print("Alex")
+months={
+"Mon": "Monday",
+"Tue": "Tuesday",
+"Wed": "Wednesday",
+"Thu": "Thursday",
+"Fri": "Friday",
+"Sat": "Saturday",
+"Sun": "Sunday"
+        }
+print(months.get("Sun"))
