4fb8ef70edd452c0f4c3c2dc5f373ba4e5d552e7
TUMWESIGYE ALEX <93213466+Tumwesigye-alex@users.noreply.github.com>
2022-03-10 17:22:06 +0300

    operators
    operators in python

---
+++ operators.py
@@ -0,0 +1,47 @@
+"""
+Arithematic operators
+-->+
+-->%
+-->/
+-->-
+"""
+num=1
+num1=2
+print(num+num1)
+"""
+>
+<
+>=
+<=
+==
+"""
+print(num<num1)
+"""
+membership
+--> in
+"""
+name='jane'
+print("a" in name)
+"""
+logial
+or
+and
+not
+"""
+n=3
+p=4
+c=1
+print(n>p or p>c)
+"""
+Assignment operators
+-=
++=
+*=
+"""
+num3=6
+num4=9
+num5=10
+sum=num3+num4+num5
+print(f"sum: {sum}")
+sum += 40
+print(sum)
